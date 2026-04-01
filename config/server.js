0575e68137d60c8491f1c1d6de40936fd6bb3fd0
mateushoeckler <mateushoeckler@gmail.com>
2026-04-01 10:06:54 -0300

    novo subdominio

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://137.131.218.195:1337',
+  url: 'https://aluno2.essenfelder.app/',
   app: {
     keys: env.array('APP_KEYS'),
   },
